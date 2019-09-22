320a68a51d486c6156ac86ad044498186813c867
pravinms <pravinms@pravinms-HP-Notebook>
2019-09-22 10:06:12 +0530

    add

---
+++ Basic/src/Logics/AnagramArrayList.java
@@ -0,0 +1,40 @@
+package Logics;
+import java.util.*;
+
+public class AnagramArrayList {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter first character");
+		String s1=sc.next();
+		System.out.println("Enter secons string");
+		String s2=sc.next();
+		s1 = s1.replaceAll("\\s", "").toLowerCase();//study this
+		s2 = s2.replaceAll("\\s", "").toLowerCase();
+		if(s1.length()!=s2.length())
+		{
+			System.out.println("Not Anagram");
+		}
+		else
+		{
+			ArrayList<Character>al1=new ArrayList<Character>();
+			ArrayList<Character>al2=new ArrayList<Character>();
+			for(int i=0;i<s1.length();i++)
+			{
+				al1.add(s1.charAt(i));
+			}
+			for(int j=0;j<s2.length();j++)
+			{
+				al2.add(s2.charAt(j));
+			}
+			Collections.sort(al1);
+			Collections.sort(al2);
+			if(al1.equals(al2))
+			{
+				System.out.println("Both are Anagrams");
+			}
+		}
+
+	}
+
+}
--- Basic/src/Logics/NotesCounterUsingArray.java
+++ Basic/src/Logics/NotesCounterUsingArray.java
@@ -1,9 +1,28 @@
 package Logics;
+import java.util.*;
 
 public class NotesCounterUsingArray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter the Rupee ");
+	int amount=sc.nextInt();
+	int note[]=new int[]{2000,500,200,100,50,20,10,5,2,1};
+	int noteindex[]=new int[note.length];
+	for(int i=0;i<note.length;i++)
+	{
+		if(amount>=note[i])
+		{
+			noteindex[i]=amount/note[i];
+			amount=amount-noteindex[i]*note[i];
+		}
+	}
+	for(int j=0;j<note.length;j++)
+	{
+		System.out.println(note[j]+" rs rupee note count is : "+noteindex[j]);
+	}
+	
+	sc.close();
 	}
 
 }
